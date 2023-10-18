d6e77ccf98d1db7ce69bb30e1c860e5fcd5cb224
dinotapislandgitlab <dinotapislandgitlab@gmail.com>
2023-10-18 19:06:58 +0200

    all petitions created

---
+++ src/main/java/org/prueba/controller/ErrorMessage.java
@@ -0,0 +1,42 @@
+package org.prueba.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.BindingResult;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter @Setter @Builder
+public class ErrorMessage {
+    private String code ;
+    private List<Map<String, String >> messages;
+
+    //No estoy utilizando la clase, pero se podría usar en el controlador para controlar los errores aquí
+    //El en update del controlador tengo comentada la linea de como se haría, se puede cambiar por la otra
+    public static String formatMessage(BindingResult result) {
+        List<Map<String, String>> errors = result.getFieldErrors().stream()
+                .map(err -> {
+                    Map<String, String> error = new HashMap<>();
+                    error.put(err.getField(), err.getDefaultMessage());
+                    return error;
+
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+}
--- src/main/java/org/prueba/controller/SuperHeroController.java
+++ src/main/java/org/prueba/controller/SuperHeroController.java
@@ -1,12 +1,77 @@
 package org.prueba.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.prueba.entity.SuperHero;
 import org.prueba.service.SuperHeroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/superheroes")
 public class SuperHeroController {
+
+    @Autowired
+    SuperHeroService superHeroService;
+
+    @GetMapping
+    public ResponseEntity<List<SuperHero>> getAllSuperHeroes() {
+        List<SuperHero> superHeroes = new ArrayList<>();
+        superHeroes = superHeroService.getAll();
+        if (superHeroes.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(superHeroes);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<SuperHero> getSuperHeroByID(@PathVariable("id") Long id) {
+        SuperHero superHero = superHeroService.getSuperHero(id);
+        if (null == superHero) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(superHero);
+    }
+
+    @GetMapping(value = "/getByName")
+    public ResponseEntity<List<SuperHero>> getByName(@RequestParam(name = "name", required = true) String name) {
+        List<SuperHero> superHeroes = new ArrayList<>();
+        superHeroes = superHeroService.getByName(name);
+        if (superHeroes.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(superHeroes);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<SuperHero> createSuperHero(@PathVariable("id") Long id, @RequestBody SuperHero superHero) {
+        superHero.setId(id);
+        SuperHero superHeroDB =  superHeroService.updateSuperHero(superHero);
+        if (superHeroDB == null){
+            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessage.formatMessage(result));
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(superHeroDB);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<SuperHero> deleteSuperHero(@PathVariable("id") Long id){
+        SuperHero superHeroDelete = superHeroService.deleteSuperHero(id);
+        if (superHeroDelete == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(superHeroDelete);
+    }
 
 }
