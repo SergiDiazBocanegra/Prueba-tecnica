84f65343d858c657fda4662cf72ff97bcee642f9
Sergi <sdiaz@isatechdata.com>
2023-10-19 09:54:10 +0200

    trying integration test

--- src/main/java/org/prueba/SuperHeroesApplication.java
+++ src/main/java/org/prueba/SuperHeroesApplication.java
@@ -1,14 +1,17 @@
 package org.prueba;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = "org.prueba")
-public class SuperHeroesApplication {
+@EnableAutoConfiguration
+public class SuperHeroesApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SuperHeroesApplication.class, args);
     }
